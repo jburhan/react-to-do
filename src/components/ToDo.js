2a6a62f0bc0c60604805b1bb93bbb1bc84f66541
jburhan <jimmyhburhan@gmail.com>
2017-11-28 13:55:44 -0800

    Checkpoint 6 react-to-do completed

---
+++ src/components/ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      // return will return JSX, a syntax for defining HTML in JS
+      <li> A todo will go here </li>
+    );
+
+  }
+
+}
+
+export default ToDo;
